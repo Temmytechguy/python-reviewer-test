9dfa10cfc21da0790a2e387e3822b9e1ee1eef7a
Temmytechguy <91670087+Temmytechguy@users.noreply.github.com>
2022-04-06 10:41:47 +0200

    Update DynamoDBScan.java
    updated the table name and aws region

--- DynamoDBScan.java
+++ DynamoDBScan.java
@@ -18,7 +18,9 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
  */
 public class DynamoDBScan implements RequestHandler<Map<String,String>, String> {
 
-    static String tableName = "<FMI1>";
+    static String tableName = "CodeGuru-MusicCollection";
+    private static AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().withRegion("us-east-1").build();
+
 
     @Override
     public String handleRequest(Map<String,String> event, Context context)
